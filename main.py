f4f6444d9d42676b5f008a79680c727c240812cf
Geronimo Gando <ggando@gmail.com>
2025-01-25 22:22:54 -0500

    Added ship and asteroids and simple movement

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        return
-    screen.fill("black")
-    pygame.display.flip()
+    pygame.init()
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
-  print("Starting asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    asteroid_field = AsteroidField()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              return
+        for sprite in updatable:
+            sprite.update(dt)
+        screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
-  main()
+    main()
